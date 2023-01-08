32e20cae02b9cd0bf548f10b2a8eb804c4dda86b
scapula07 <bartholomewonogwu@yahoo.com>
2023-01-08 22:13:52 +0100

    new

--- config-overrides.js
+++ config-overrides.js
@@ -12,7 +12,6 @@ module.exports = function override(config) {
         "os": require.resolve("os-browserify"),
         "url": require.resolve("url"),
         'process/browser': require.resolve('process/browser'),
-   
         "path": require.resolve("path-browserify")
     })
     config.resolve.fallback = fallback;
